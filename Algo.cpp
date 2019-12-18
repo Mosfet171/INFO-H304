44a9797cb2d7aecd6926de38a4a5f0ecbb7c6076
JCNdox <57673610+JCNdox@users.noreply.github.com>
2019-12-18 16:17:00 +0100

    Update Algo.cpp

--- Algo.cpp
+++ Algo.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 Algo::Algo(string *query, string *target, Blosum *blosum) : m_query(query), m_target(target), m_p((*query).size()), m_q((*target).size()), m_blosum(blosum), m_max(0)//, m_count(0)
 {
-    constructionMatrice(); //construction de la matrice de similaritÈ
-    valeursMatrice(0,0);
+    constructionMatrice(); //construction de la matrice de similarit√©
+    valeursMatrice();
 }
 
 Algo::Algo(Algo const& clone) //constructeur par copie
@@ -25,7 +25,7 @@ Algo::~Algo() //destructeur
     delete[] m_matrice;
 }
 
-//construit une matrice qui contiendra les scores de similartiÈ et une autre qui contiendra les scores d'algo
+//construit une matrice qui contiendra les scores de similarti√© et une autre qui contiendra les scores d'algo
 void Algo::constructionMatrice()
 {
     m_matrice = new int*[m_p];
@@ -33,54 +33,23 @@ void Algo::constructionMatrice()
     {
         m_matrice[i] = new int[m_q];
     }
-    /*for(int i = 0; i < m_p; i++)
-    {
-        for(int j = 0; j < m_q; j++)
-        {
-            m_matrice[i][j] = 0;
-        }
-    }*/
 }
 
 //remplissage de la matrice des scores
 void Algo::valeursMatrice(int p, int q)
 {
-    int a(p), b(q);
-    for(int i = p; i < m_p; i++)
+    for(int i = 0; i < m_p; i ++)
     {
-        m_matrice[i][q] = bestChoice(i, q);
-
-        if(max(m_max, m_matrice[i][q]) == m_matrice[i][q])
+        for(int j = 0; j < m_q; j++)
         {
-            m_max = m_matrice[i][q];
+            m_matrice[i][j] = bestChoice(i,j);
+            m_max = max(m_max, m_matrice[i][j]);
         }
-    }
-    for(int j = q + 1; j < m_q; j++)
-    {
-        m_matrice[p][j] = bestChoice(p, j);
-
-        if(max(m_max, m_matrice[p][j]) == m_matrice[p][j])
-        {
-            m_max = m_matrice[p][j];
-            //cout << m_max << endl;
-        }
-    }
-    if(p < m_p - 1)
-    {
-        a = p + 1;
-    }
-    if(q < m_q - 1)
-    {
-        b = q + 1;
-    }
-    if((a-p)+(b-q) != 0)
-    {
-        valeursMatrice(a, b);
     }
 }
 
-//On remplit la matrice en fonction des scores des cases prÈcÈdentes
-//Si la case prÈcÈdente est sur la diagonale ==> pas de pÈnalitÈ
+//On remplit la matrice en fonction des scores des cases pr√©c√©dentes
+//Si la case pr√©c√©dente est sur la diagonale ==> pas de p√©nalit√©
 int Algo::bestChoice(int a, int b)
 {
     int solution(0), solution1(0), solution2(0), solution3(0);
